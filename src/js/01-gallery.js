7958f7bf4033e864fccdfa470573ca8521665225
VolodymyrTrush <vmtrush@gmail.com>
2022-05-01 21:43:57 +0300

    HW8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -33,3 +33,6 @@ const lightbox = new SimpleLightbox('.gallery a', {
         captionsData: 'alt',
         captionDelay: '250ms',
 });
+// -------------------------------------------------------------------------------
+
+
