1ce8c897bfb2b4f38acdf598725f82e9990af5c3
VolodymyrTrush <vmtrush@gmail.com>
2022-05-01 21:32:59 +0300

    HW8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -12,13 +12,19 @@ const galleryMarkup = createGalleryItems(galleryItems);
 
 function createGalleryItems(galleryItems) {
      
-    return galleryItems.map(({ preview, original, description, }) => {
-      return `<li>
-                <a class="gallery__item" href="${original}">
-                  <img class="gallery__image" src="${preview}" alt="${description}"/>
-                </a>
-              </li>`
-    }).join("");  
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`
+    }
+    ).join("");     
 };
 
 galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
@@ -27,6 +33,3 @@ const lightbox = new SimpleLightbox('.gallery a', {
         captionsData: 'alt',
         captionDelay: '250ms',
 });
-
-
-
