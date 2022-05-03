b1c8f70f506730dd0f3ab0424ade3b6b96d9214d
VolodymyrTrush <vmtrush@gmail.com>
2022-05-03 12:58:18 +0300

    HW8
    Task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from "lodash.throttle";
 
+const STORAGE_KEY = "feedback-form-state";
+
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    textarea: document.querySelector(".feedback-form textarea"),
+};
+
+refs.textarea.addEventListener('input', onInputTextareaSave);
+refs.form.addEventListener('submit', throttle(onFormSubmit, 500));
+
+populateInputTextarea();
+
+function onInputTextareaSave(event) {
+    const massage = event.carrentTarget.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(massage));
+};
+
+function populateInputTextarea() {
+    const saveMessage = localStorage.getItem(STORAGE_KEY);
+    if (saveMessage) {
+       refs.textarea.value= saveMessage;
+    };
+};
+
+function onFormSubmit(event) {
+    event.preventDefault(); 
+
+    const { email, message } = event.currentTarget.elements;
+    console.log({ email, message });
+
+    localStorage.removeItem(STORAGE_KEY);
+    event.currentTarget.reset();
+};
