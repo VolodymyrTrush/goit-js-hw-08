dfef01a3fd4feec7569298103a6b1cc07ab51454
VolodymyrTrush <vmtrush@gmail.com>
2022-05-05 20:07:33 +0300

    HW8
    Task2 исправлено

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,34 +1,17 @@
-import Player from "@vimeo/player";
-import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+const saveTime = localStorage.getItem(STORAGE_KEY);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
-
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
-
-    const onPlay = function(data) {
-    localStorage.setItem("videoplayer-current-time", data.seconds);
+const onPlay = data => {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
 };
+
 player.on('play', throttle(onPlay, 1000));
 
-const saveTime = localStorage.getItem("videoplayer-current-time");
-player.setCurrentTime(Number(saveTime)).then(function(seconds) {
-    seconds = Number(saveTime);
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            console.log('Error time');
-            break;
-
-        default:
-            console.log('Error other');
-            break;
-    }
+player.setCurrentTime(+saveTime).then(function (seconds) {
+  seconds = +saveTime;
 });
-
